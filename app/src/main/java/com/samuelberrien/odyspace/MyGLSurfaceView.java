54aa9a0a0c2604c325b49372615fe7e489d28809
samuel <berriens@gmail.com>
2017-04-19 17:17:14 +0200

    rotation ship OK

--- app/src/main/java/com/samuelberrien/odyspace/MyGLSurfaceView.java
+++ app/src/main/java/com/samuelberrien/odyspace/MyGLSurfaceView.java
@@ -15,10 +15,6 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
     private MyGLRenderer renderer;
 
-    private final float TOUCH_SCALE_FACTOR_MOVE = 0.001f;
-    private float mPreviousX;
-    private float mPreviousY;
-
     /**
      * @param context
      */
@@ -32,26 +28,11 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
-
         this.renderer.updateMotion(e);
-        /*switch (e.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mPreviousX = e.getX() + 1f;
-                mPreviousY = e.getY() + 1f;
-                this.renderer.setJoystickCenter(-(2f * e.getX() / this.getWidth() - 1f), -(2f * e.getY() / this.getHeight() - 1f));
-            case MotionEvent.ACTION_MOVE:
-                float dx = e.getX() + 1f - mPreviousX;
-                float dy = e.getY() + 1f - mPreviousY;
-                renderer.updateCameraOrientation(dy * TOUCH_SCALE_FACTOR_MOVE, dx * TOUCH_SCALE_FACTOR_MOVE);
-                requestRender();
-        }
-        mPreviousX = e.getX() + 1f;
-        mPreviousY = e.getY() + 1f;*/
         return true;
     }
 
     public void onPause() {
-
         super.onPause();
     }
 
