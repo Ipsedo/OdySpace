44b503a19c05334d8bf2e03e3e9167cb8055e569
samuel <berriens@gmail.com>
2017-04-23 23:26:14 +0200

    add down limit end game

--- app/src/main/java/com/samuelberrien/odyspace/MyGLSurfaceView.java
+++ app/src/main/java/com/samuelberrien/odyspace/MyGLSurfaceView.java
@@ -21,9 +21,9 @@ public class MyGLSurfaceView extends GLSurfaceView {
     public MyGLSurfaceView(Context context) {
         super(context);
         // Create an OpenGL ES 2.0 context.
-        setEGLContextClientVersion(2);
+        this.setEGLContextClientVersion(2);
         this.renderer = new MyGLRenderer(context, this);
-        setRenderer(this.renderer);
+        this.setRenderer(this.renderer);
     }
 
     @Override
