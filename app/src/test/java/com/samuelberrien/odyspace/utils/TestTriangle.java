8ebd84182fc1a19ac24526cc96cc29f6125a8cb4
samuel <berriens@gmail.com>
2017-04-24 03:16:49 +0200

    implemented collision method -> test failed

--- app/src/test/java/com/samuelberrien/odyspace/utils/TestTriangle.java
+++ app/src/test/java/com/samuelberrien/odyspace/utils/TestTriangle.java
@@ -25,18 +25,20 @@ public class TestTriangle {
         double[] v1 = new double[]{0.9d, 3, 0.8};
         double[] v2 = new double[]{3, 2, 1.5};
 
-        assertTrue(Triangle.tr_tri_intersect3D(u0, u1, u2, v0, v1, v2) == 0);
+        assertTrue(Triangle.tr_tri_intersect3D(u0.clone(), u1.clone(), u2.clone(), v0.clone(), v1.clone(), v2.clone()) == 0);
+        assertTrue(Triangle.tr_tri_intersect3D(v0.clone(), v1.clone(), v2.clone(), u0.clone(), u1.clone(), u2.clone()) == 0);
 
         assertTrue(Triangle.tr_tri_intersect3D(u0.clone(), u1.clone(), u2.clone(), u0.clone(), u1.clone(), u2.clone()) > 0);
 
         u0 = new double[]{0d, 0d, 1d};
         u1 = new double[]{1d, 0d, 0d};
-        u2 = new double[]{0d, 1d, 0d};
+        u2 = new double[]{0d, 0d, -1.32d};
 
-        v0 = new double[]{0.5d + 0d, 0.5d + 0d, 1d};
-        v1 = new double[]{0.5d + -1d, 0.5d + 0d, 0d};
-        v2 = new double[]{0.5d + 0d, 0.5d + -1d, 0d};
+        v0 = new double[]{-3d, 3d, 0d};
+        v1 = new double[]{1.66d, -4.22d, 0.65d};
+        v2 = new double[]{1.96d, 3.49d, 0.14d};
 
-        assertTrue(Triangle.tr_tri_intersect3D(u0, u1, u2, v0, v1, v2) > 0);
+        assertTrue(Triangle.tr_tri_intersect3D(u0.clone(), u1.clone(), u2.clone(), v0.clone(), v1.clone(), v2.clone()) > 0);
+        assertTrue(Triangle.tr_tri_intersect3D(v0.clone(), v1.clone(), v2.clone(), u0.clone(), u1.clone(), u2.clone()) > 0);
     }
 }
