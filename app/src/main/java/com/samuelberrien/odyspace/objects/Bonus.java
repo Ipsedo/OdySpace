9e478cda159fb2abc97ca01914e0c5d87a0884f4
samuel <berriens@gmail.com>
2017-05-03 00:41:23 +0200

    need to make Bonus

---
+++ app/src/main/java/com/samuelberrien/odyspace/objects/Bonus.java
@@ -0,0 +1,20 @@
+package com.samuelberrien.odyspace.objects;
+
+import android.content.Context;
+
+/**
+ * Created by samuel on 03/05/17.
+ * Copyright samuel, 2016 - 2017.
+ * Toute reproduction ou utilisation sans l'autorisation
+ * de l'auteur engendrera des poursuites judiciaires.
+ */
+
+public class Bonus extends BaseItem {
+    public Bonus(Context context, String objFileName, String mtlFileName, float[] mPosition, float[] mSpeed, float[] mAcceleration) {
+        super(context, objFileName, mtlFileName, 1f, 0f, 1, mPosition, mSpeed, mAcceleration);
+    }
+
+    public void decrementsBothLife(BaseItem other){
+        this.life -= other.life;
+    }
+}
