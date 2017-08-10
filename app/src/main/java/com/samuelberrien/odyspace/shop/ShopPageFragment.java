96a369fba7c7a7c06821571fdd81fbc501ae9a0d
Ipsedo <berriens@gmail.com>
2017-08-11 01:39:58 +0200

    new style shop button

--- app/src/main/java/com/samuelberrien/odyspace/shop/ShopPageFragment.java
+++ app/src/main/java/com/samuelberrien/odyspace/shop/ShopPageFragment.java
@@ -37,7 +37,6 @@ public class ShopPageFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_page, container, false);
 		return ((ShopActivity) getActivity()).setPageChosen(this.mPage - 1, inflater, container);
 	}
 }
